be72d167d9648b243251f194cbf4d54d09a86af1
Jonathan Villanueva <villanuevajonathan606@gmail.com>
2021-07-08 21:33:55 -0700

    commit tree bf6629a317489e1153a225dda4d90ee15af471d6

--- src/components/App.js
+++ src/components/App.js
@@ -11,10 +11,14 @@ function App() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
 
+  function handleFormSubmit(NewItem){
+    setItems([...items, NewItem]); 
+  }
+
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} />
+      <ShoppingList items={items} onItemFormSubmit = {handleFormSubmit} />
     </div>
   );
 }
